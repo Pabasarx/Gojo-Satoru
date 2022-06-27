5af2b41f8a9e0b09f9ed3e613767f53463b02aa6
Pabasarx <108265284+Pabasarx@users.noreply.github.com>
2022-06-27 14:41:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94761327016']
+global.premium = ['94761327016']
+global.ownernomer = 'Pabasarajd'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
